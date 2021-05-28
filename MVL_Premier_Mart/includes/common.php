4911eb65a2a3fcbcc18deaf0ed475f1309b300cf
VEERA VP <veeravp11@gmail.com>
2021-05-28 14:47:09 +0530

    Add files via upload

---
+++ MVL_Premier_Mart/includes/common.php
@@ -0,0 +1,4 @@
+<?php
+$con= mysqli_connect("localhost","root","","mvl_premier_mart") or die(mysqli_error($con));
+session_start();
+?>
---
+++ MVL_Premier_Mart/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container">
+        <p style="font-size:16px;" align="center">Write to us : MVLPremierMart@gmail.com | Contact Us : +91 9876543210</p>
+    </div>
+</footer>
